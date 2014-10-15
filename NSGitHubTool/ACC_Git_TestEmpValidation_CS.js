29f6682752fb851acfef02a1b9c1e5dbab1e48d2
unitha.rangam <unitha.rangam@BDC6-L-DMGWTQ1.GDNINDIA.COM>
2014-10-15 17:26:49 +0530

    First Time Commit 1015.520

---
+++ NSGitHubTool/ACC_Git_TestEmpValidation_CS.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       15 Oct 2014     unitha.rangam
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @returns {Boolean} True to continue save, false to abort save
+ */
+function clientSaveRecord(){
+
+    return true;
+}
